c1554db1ad36884ecc5d258d5f7c79f31240291a
Lanceboy01 <98538058+Lanceboy01@users.noreply.github.com>
2022-01-29 08:35:01 +0800

    pull test

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("Hello master");
         System.out.println("Hello push to git-test01");
         System.out.println("Hello ssh push to git-test01");
+        System.out.println("Hello ssh pull to git-test01");
 
     }
 }
